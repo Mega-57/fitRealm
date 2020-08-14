a0e616ffefeed52623dcf07df9748c69084b772d
Mega-57 <l.cnor752@gmail.com>
2020-08-13 19:13:24 -0700

    FitRealm App v. 1.051: Landing Page style tweaks

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { MbscModule } from '@mobiscroll/angular';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+
+ 
+import 'chartjs-plugin-zoom';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [ 
+    MbscModule,  
+    FormsModule,  
+BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot(), ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
